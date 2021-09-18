aefb8d2611707ab07b4506794498c89827407f88
SugVes <guu.allvs@gmail.com>
2021-09-17 22:56:46 -0300

    Criado model para parse de arquivo

---
+++ src/main/java/com/bycoders/model/TransactionDocument.java
@@ -0,0 +1,85 @@
+package com.bycoders.model;
+
+import com.bycoders.enums.TransactionType;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class TransactionDocument {
+
+    private TransactionType type;
+    private LocalDate transactionDate;
+    private double transactionValue;
+    private String cpf;
+    private String card;
+    private LocalTime hour;
+    private String storeOwner;
+    private String storeName;
+
+    public TransactionDocument() {
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public double getTransactionValue() {
+        return transactionValue;
+    }
+
+    public void setTransactionValue(double transactionValue) {
+        this.transactionValue = transactionValue;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public LocalTime getHour() {
+        return hour;
+    }
+
+    public void setHour(LocalTime hour) {
+        this.hour = hour;
+    }
+
+    public String getStoreOwner() {
+        return storeOwner;
+    }
+
+    public void setStoreOwner(String storeOwner) {
+        this.storeOwner = storeOwner;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+}
